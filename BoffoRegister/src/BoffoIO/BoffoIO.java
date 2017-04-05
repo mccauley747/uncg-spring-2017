469be291716064e38552f53b30b1d2887c659d8a
Spencer Whyatt <sjwhyatt@uncg.edu>
2017-04-05 15:26:05 -0400

    Change package name and added logic to IO
    changed the package name of BoffoIO to boffoio and added method to read
    in a barcode and return it as an int.

--- BoffoRegister/src/BoffoIO/BoffoIO.java
+++ BoffoRegister/src/BoffoIO/BoffoIO.java
@@ -1,12 +1,17 @@
-package BoffoIO;
+package boffoio;
 
 /**
  *
  * @author sjwhyatt
  */
-
 import java.util.Scanner;
 
 public class BoffoIO {
-    
+
+    protected Scanner input;
+
+    public int scanBarcode(){
+        int barcode = input.nextInt();
+        return barcode;
+    }
 }
